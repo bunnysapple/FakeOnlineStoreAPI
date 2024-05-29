3a6f26d861f49389703e8b89da0a483e00a81ec8
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-05-29 23:39:51 +0100

    add comments explaining the code

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_all_products():
 # I've passed in a "query" variable into the get_search function.
 @app.route("/search/<query>/")
 def get_search(query:str):
-    # get all the parameters as variables and assign any relevant variables as necessary
+    # get all the passed in values as variables and assign any relevant variables as necessary
     filter_type = request.args.get("filter_type", default="all", type=str)
     page = request.args.get("page", default=1, type=int)
     per_page = request.args.get("per_page", default=10, type=int)
@@ -82,38 +82,57 @@ def get_search(query:str):
             "total_products": num_products,
             "products": sort_lst[start:end] if len(sort_lst) >= end else sort_lst[start:]
             }
-        # return the data in json format.
+        # return the result in json format.
         return jsonify(result)
 
+# This route allows for you to search for a specific product using it's id
+# You have to pass in the id through the URL
 @app.route("/id/<id>/")
 def get_id(id:int):
+    # all the ids that I've used in the data are integers and so the id passed in must be a numeric string
+    # if not a numeric string, it will return a 404 status code
     if not id.isdigit():
-        return {}, 404
+        return 404
     id = int(id)
+    # get the file path for data.json and open and load the json file as data
     filename = os.path.join(os.path.dirname(__file__), "data.json")
     with open(filename) as data:
         data = json.load(data)
         products = data["products"]
+        # if the id passed in is not found in the products, return an empty dict and a 204 status code
         if id > len(products) or id < 0:
             return {}, 204
-        results = {
+        # since the id values are ints and represent the index values of the products in my data,
+        # retreiving the product is pretty straightforward list value retrieval
+        result = {
             "page": 1,
             "pages": 1,
             "total_products": 1,
             "products": products[id]
         }
-        return jsonify(results), 200
+        return jsonify(result), 200
 
+# This route allows for you to find similar or related products in the data and returns them
+# All necessary values are passed in as parameters through the URL
 @app.route("/similar/")
 def get_similar():
+    # get all the passed in values as variables and assign any relevant variables as necessary
     id = request.args.get("id", default=-1, type=int)
     search = request.args.get("search", default="", type=str)
     per_page = request.args.get("per_page", default=5, type=int)
+    if per_page > 10:
+        per_page = 10
+    elif per_page < 5:
+        per_page = 5
+    # get the file path for data.json and open and load the json file as data
     filename = os.path.join(os.path.dirname(__file__), "data.json")
     with open(filename) as data:
         data = json.load(data)
         products = data["products"]
+        # get the keywords that are to be compared to find closely related products
         search_for = next((x["keywords"] for x in products if x["id"] == id), search.split())
+        # get a list of dicts wich contain the product and a score on their relevance
+        # their relevance is based on how many of the keywords match
         sort_lst = [
             {"product": x,
              "relevance": len(
@@ -122,15 +141,13 @@ def get_similar():
                  ]
             )
             } for x in products]
-        # sort_lst = [{"product": x, "relevance": sum(
-        #     jaro_winkler(z, y) for z in search_for for y in x["keywords"]
-        # )} for x in products]
+        # sort the list based on the relevance and then get a limited number of them to be returned
         sort_lst.sort(key=itemgetter("relevance"), reverse=True)
-        products = sort_lst[0:per_page+1] if len(sort_lst) >= per_page else sort_lst
+        sort_lst = sort_lst[0:per_page+1] if len(sort_lst) >= per_page else sort_lst
         results = {
-            "products": {"products": [x["product"] for x in products[1:]]}
+            "products": {"products": [x["product"] for x in sort_lst[1:]]}
         }
-        return jsonify(results)
+        return jsonify(results), 200
 
 
 if __name__ == "__main__":
